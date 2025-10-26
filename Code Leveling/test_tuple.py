6fa4e9701359181e15fe7d20cb06e7239d761b60
cassliya2307 <haliyakareem23@gmail.com>
2025-10-26 20:29:06 +0100

    Add Code Leveling

---
+++ Code Leveling/test_tuple.py
@@ -0,0 +1,85 @@
+import unittest
+from unmodified_tuple import*
+class TestToAddNumberToATupleWithoutModfyingIt(unittest.TestCase):
+
+	def test_if_there_are_numbers_in_the_tuple_is_the_same_with_the_returned_value(self):
+		actual = unmodified_tuple(9, 1, [9],3 ,4)
+		
+		expected = 9, 1, [9, 5] ,3 ,4
+
+		self.assertEqual(actual, expected)
+
+
+	
+	def test_if_the_returned_value_has_a_list_with_two_elements(self):
+		actual = modified_list(9, 1, [9, 9],3 ,4)
+		
+		expected = 9, 1, [9, 99] ,3 ,4
+
+		self.assertEqual(actual, expected)
+
+		
+	def test_if_the_value_is_a_list(self):
+		actual = convert_tuple_to_list("apple" , "banana" )
+		expected = ["apple" , "banana" , "mango"]
+		self.assertEqual(actual, expected)
+
+	
+	def test_for_the_return_type_of_the_twoD_list(self):
+		actual = add_inner_list([[2, 3, 4], [1,5,7] , [4,6,8]]
+)
+		expected = [9, 13, 18]
+		self.assertEqual(actual, expected)
+
+
+
+
+	def test_for_the_return_type_of_the_second_twoD_list(self):
+		actual = add_inner_elements_with_the_same_indexes([ [2, 3, 4],  [1, 5, 7],  [4, 6, 8] ]
+)
+		expected =  [7, 14, 19]
+		self.assertEqual(actual, expected)
+
+	def test_for_accurate_return_value(self):
+		actual = filter_even_numbers(4)
+	
+		expected = 4
+		
+		self.assertEqual(actual, expected)
+
+
+
+	#def test_that_it_returns_strings_with_only_three_characters(self):
+		#actual = get_string_less_than_four(["naomi" , "cat" , "salmon", "rat"])
+		#expected = ["cat" , "rat"]
+		#self.assertEqual(actual, expected)
+
+	
+	#def test_that_it_returns_tuple_first_value_greater_than_2(self):
+		#actual = get_grades_in_tuple([(1, "A") , (2 , "B") , (3 , "C") , (4, "D")])
+		#expected = [(2 , "B") , (3 , "C") , (4, "D")]
+		#self.assertEqual(actual, expected)
+
+	
+
+
+	def test_for_accurate_return_value_two(self):
+		actual = filter_multiple_of_three_and_five(15)
+	
+		expected = 15
+		
+		self.assertEqual(actual, expected)
+
+
+
+	#def test_for_return_value(self):
+		#actual = get_pallindrum(["level" , "madam" , "love"])
+		#expected = ["level" , "madam"]
+		#self.assertEqual(actual, expected)
+
+
+	def test_if_the_return_type_is_uppercase(self):
+		actual = to_uppercase(["code"])
+		expected = CODE
+		self.assertEqual(actual, expected)
+
---
+++ Code Leveling/unmodified_tuple.py
@@ -0,0 +1,138 @@
+def unmodified_tuple(*number):
+	number[2].append(5)
+	return number
+	
+
+
+def modified_list(*number):
+	number[2].pop()
+	number[2].append(99)
+	return number
+
+def convert_tuple_to_list(*fruits):
+	result = []
+	result2 = []
+	result = list(fruits)
+	result2 = result.append("mango")
+	return result
+
+def unpack_tuple(*numbers):
+	number1 = numbers[0]
+	number2 = numbers[1]
+	others	= numbers[2: : 1]
+	print(number1 , end = " ")
+	print(number2 , end = " ")
+	print(others , end = " ")
+	return ""
+
+def add_inner_list(numbers):
+	all_numbers = []
+	count = 0
+	count2 = 0
+	count3 = 0
+	digit = 0
+	digit2 = 0
+	digit3 = 0
+	for num in numbers:
+		digit += numbers[0][count]
+		count += 1
+	all_numbers.append(digit) 
+	for num in numbers:
+		digit2 += numbers[1][count2]
+		count2 += 1
+	all_numbers.append(digit2) 
+	
+	for num in numbers:
+		digit3 += numbers[2][count3]
+		count3 += 1
+	all_numbers.append(digit3) 
+	
+	return all_numbers
+
+def add_inner_elements_with_the_same_indexes(numbers):
+	new_list = []
+	count = 0
+	digit = 0
+	digit1 = 0
+	digit2 = 0
+	for num in numbers:
+		digit += num[count]
+		digit1 += num[count + 1]
+		digit2 += num[count + 2]
+		
+	new_list.append(digit)
+	new_list.append(digit1)
+	new_list.append(digit2)
+	return new_list	
+
+def filter_even_numbers(number):
+	if number % 2 == 0 and number <= 21 and number >= 1:
+		return number	
+
+def get_string_less_than_four(strings):
+	count = 0
+	for words in range(len(strings)):
+		count += 1
+		if strings[count] <= 3:
+			return strings	
+
+
+#def get_grades_in_tuple(number_grade):
+	#count = -1
+	#for num in number_grade:
+		#count += 1
+		#print(number_grade[count])
+		#if number_grade[count] == int and number_grade[count] >= 2:
+			#return number_grade[num]
+
+
+def filter_multiple_of_three_and_five(number):
+	if number % 3 == 0 and number % 5 == 0 and number <= 51 and number >= 1:
+		return number	
+
+
+
+#def get_pallindrum(words):
+	#reverse = 
+	#for word in words:
+		
+
+
+def to_uppercase(words):
+	count = -1
+	new_list = []
+	for word in words:
+		count += 1
+		new_list.append(words[count].upper())
+	return new_list	
+
+number = 6, 8, [8], 9, 0
+#print("1." ,unmodified_tuple(*number))
+
+num = 1, 2, [3,4], 5
+#print("2." ,modified_list(*number))
+
+fruits = "apple" , "pineapple", "banana", "orange"
+#print("3." ,convert_tuple_to_list(*fruits))
+
+numbers = 10, 20, 30,40
+#print(unpack_tuple(*numbers))
+
+
+numbers = [[2, 3, 4], [1,5,7] , [4,6,8]]
+#print(add_inner_list(numbers))
+
+
+print(add_inner_elements_with_the_same_indexes(numbers))
+
+number = [1, 45, 6, 8, 9,2, 3,5,68 ,8,20,3,5,78,3,57,2,6,20,18]
+output = list(filter(filter_even_numbers, number))
+print(output)
+
+words = ["puppy" , "kitty" , "turtle"]
+output = list(filter(to_uppercase, words))
+print(output)
+
+
+
+
